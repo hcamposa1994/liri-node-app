41399fa2a355ba7c6c9c2fa2bdeaf9bafd20cd29
Herbert Campos <wohoforever@yahoo.com>
2019-06-17 11:51:19 -0700

    researching how spotify node api works

--- liri.js
+++ liri.js
@@ -4,3 +4,12 @@ var keys = require("./keys");
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
+
+spotify
+  .search({ type: 'track', query: 'Despacito', limit: 1 })
+  .then(function(response) {
+    console.log(JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
+})
+  .catch(function(err) {
+    console.log(err);
+  });
