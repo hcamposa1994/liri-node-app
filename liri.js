b8e6dbfb24cc14da920d6cb7137b9207f16a5981
Herbert Campos <wohoforever@yahoo.com>
2019-06-17 19:38:00 -0700

    finished the bands api implementation icluding user input

--- liri.js
+++ liri.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 var keys = require("./keys");
 
 var Spotify = require('node-spotify-api');
-
-var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require('moment');
 
 var argument = process.argv[2];
 
 if(argument === "spotify-this-song")
 {
+    var spotify = new Spotify(keys.spotify);
     var song = process.argv[3] || "The Sign";
     console.log("--SPOTIFY--")
     spotify
@@ -34,3 +35,34 @@ if(argument === "spotify-this-song")
         console.log(err);
     });
 };
+
+if(argument === "concert-this")
+{
+    var artist = process.argv[3];
+    console.log("--BANDSINTOWN--")
+
+    axios
+    .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+
+        for (let i = 0; i < response.data.length; i++) {
+            console.log("\n---BAND/ARTIST INFO---")
+            console.log("Venue name: " + response.data[i].venue.name);
+            console.log("Venue location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+            console.log("Event time: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+        }
+
+    })
+    .catch(function(error) {
+        if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        } else if (error.request) {
+        console.log(error.request);
+        } else {
+        console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
+}
